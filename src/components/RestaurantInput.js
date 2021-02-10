4c69dff514ee825d992688f84e1a043da56de74e
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-10 19:18:13 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -52,3 +52,13 @@ export class RestaurantInput extends Component {
 
 //connect this component by wrapping RestaurantInput below
 
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: (newRestaurant) => {
+      dispatch(addRestaurant(newRestaurant))
+    }
+  };
+};
+
+
+export default connect(null, mapDispatchToProps)(RestaurantInput);
